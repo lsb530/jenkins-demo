dc09ae9ce9fefcd1f48311dc39e3ae7f5c872bba
boki <lsb530@naver.com>
2025-06-29 23:55:55 +0900

    도커 소켓 연결

--- jenkins-21.sh
+++ jenkins-21.sh
@@ -1,4 +1,5 @@
 docker run -d -p 8088:8080 -p 9090:9090 -p 50000:50000 --name jenkins-server \
 	--restart=on-failure \
+  -v /var/run/docker.sock:/var/run/docker.sock \
 	-v jenkins_home:/var/jenkins_home \
 	jenkins/jenkins:lts-jdk21
