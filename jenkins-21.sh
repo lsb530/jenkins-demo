38bd8a42b819230137aaba0b64234bcfc2fd84c3
boki <lsb530@naver.com>
2025-06-30 11:13:52 +0900

    docker install shell

--- jenkins-21.sh
+++ jenkins-21.sh
@@ -1,5 +1,7 @@
 docker run -d -p 8088:8080 -p 9090:9090 -p 50000:50000 --name jenkins-server \
 	--restart=on-failure \
+  --privileged \
   -v /var/run/docker.sock:/var/run/docker.sock \
 	-v jenkins_home:/var/jenkins_home \
+	-u root \
 	jenkins/jenkins:lts-jdk21
